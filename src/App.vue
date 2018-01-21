55f71a01d2a4cf744f73b31cc7866204031d9819
Oliver Reid <reiol787@student.otago.ac.nz>
2018-01-21 18:28:33 +1300

    Github setup

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">{{ title }}</div>
+    </v-ons-toolbar>
+
+    <v-ons-tabbar position="auto"
+      :tabs="tabs"
+      :visible="true"
+      :index.sync="activeIndex"
+    >
+    </v-ons-tabbar>
+  </v-ons-page>
+</template>
+
+<script>
+  import homePage from 'Home';
+  import explorePage from 'Explore';
+
+  export default {
+    data() {
+      return {
+        activeIndex: 0,
+        tabs: [
+	  {
+	    icon: this.md() ? null : 'ion-home',
+	    label: 'Home',
+	    page: homePage
+	  },
+	  {
+	    icon: this.md() ? null : 'ion-location',
+	    label: 'Explore',
+	    page: explorePage
+	  }
+        ]
+      };
+    },
+    methods: {
+      md() {
+        return this.$ons.platform.isAndroid();
+      }
+    },
+    computed: {
+      title() {
+        return this.tabs[this.activeIndex].label;
+      }
+    },
+    components: { homePage, explorePage }
+  }
+</script>
---
+++ src/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-ons-page>
+
+    <v-ons-card v-for="post in posts" :key="post.id">
+      <!-- <img src="{{post.image}}" alt="Onsen UI" style="width: 100%"> -->
+      <div class="title">
+        {{post.title}}
+      </div>
+      <div class="content">
+        <p>{{post.body}}</p>
+      </div>
+    </v-ons-card>
+
+  </v-ons-page>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      apiUrl: 'https://api.thoughtful.kiwi/',
+      posts: []
+    }
+  },
+  methods: {
+    getPosts() {
+      axios.get(this.apiUrl + 'post/')
+        .then(response => this.posts = response.data)
+    }
+  },
+  created() {
+    this.getPosts();
+  }
+  
+}
+</script>
